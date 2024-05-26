0821b079857cb4faac35b13ae17bd1ab4f72a4fa
ahmarshahid <ahmarjan456@gmail.com>
2024-05-26 23:47:43 +0500

    Complete Healthcare Data Analysis Project using React and SQL Database

--- backend/dbconnections/dbconfig.js
+++ backend/dbconnections/dbconfig.js
@@ -1,14 +1,14 @@
 const config = {
-    user : 'tayyba',
-    password : '123',
-    server : 'TAYYBAPC',
-    database : 'FINAL ERD',
-    options : {
-        trustServerCertificate : true,
-        trustedConnection : false,
-        enableArithAbort : true,
-        instancename : null
-    },
-    port : 1433
-}
+  user: "AHMAR",
+  password: "12345",
+  server: "AHMAR",
+  database: "FINALERD",
+  options: {
+    trustServerCertificate: true,
+    trustedConnection: false,
+    enableArithAbort: true,
+    instancename: null,
+  },
+  port: 1433,
+};
 module.exports = config;
